8058924b31c6620ccaef353b83ad9de74cd475d9
brendon-ai <brendon-m@outlook.com>
2017-09-10 12:38:17 -0400

    Un-reversed rows and columns, now look for greatest prediction value in row

--- infer/sliding_window_inference_engine.py
+++ infer/sliding_window_inference_engine.py
@@ -23,34 +23,46 @@ class SlidingWindowInferenceEngine:
         self.window_size = window_size
         self.stride = stride
 
-    # Given an image, compute a Boolean matrix containing the network's classifications of the corresponding window
+    # Given an image, compute a vector of positions describing the position of the line for each column
     def infer(self, image):
 
         # Slice up the image into windows
         image_slices = view_as_windows(image, (self.window_size, self.window_size, 3), self.stride)
 
-        # A two-dimensional list that will contain the predictions corresponding to the windows
-        classification_matrix = []
+        print(image_slices.shape)
+
+        # A two-dimensional list that will contain the line positions for each column
+        line_positions = []
 
         # Loop over the windows and classify them, one row at a time
-        for row in range(image_slices.shape[0]):
+        for column in range(image_slices.shape[1]):
 
-            # Create a one-dimensional list of the classifications for this row
-            row_classifications = []
+            # Create a one-dimensional list of the predictions for this row
+            row_predictions = []
 
             # Loop over the second dimension
-            for column in range(image_slices.shape[1]):
+            for row in range(image_slices.shape[0]):
 
                 # Slice the individual window out of the list
                 window = image_slices[row, column]
 
                 # Use the model to classify the image, and convert the result to a Boolean
-                classification = bool(round(self.model.predict(window)[0, 0]))
+                prediction = self.model.predict(window)[0, 0]
 
                 # Append it to the list of classifications for this row
-                row_classifications.append(classification)
+                row_predictions.append(prediction)
 
-            # Append this row's classifications to the matrix for the entire image
-            classification_matrix.append(row_classifications)
+            # We must now compute the position of the line based on the list of classifications for the row
+            # Find the max value in the list (the most likely to be a line)
+            max_prediction = max(row_predictions)
 
-        return classification_matrix
+            # Find the index of the max value
+            max_prediction_index = row_predictions.index(max_prediction)
+
+            # Using the stride and window size, compute the horizontal position corresponding to the index
+            max_horizontal_position = (self.window_size // 2) + (self.stride * max_prediction_index)
+
+            # Add it to the list of positions
+            line_positions.append(max_horizontal_position)
+
+        return line_positions
