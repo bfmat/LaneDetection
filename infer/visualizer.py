0a2d57e261161229300d16cdda31d65351b6dbca
brendon-ai <brendon-m@outlook.com>
2017-09-17 15:41:21 -0400

    Visualizer now displays metadata about images as they are displayed

--- infer/visualizer.py
+++ infer/visualizer.py
@@ -1,10 +1,12 @@
+from __future__ import print_function
+
 import os
 import sys
 
 from scipy import misc
 from keras.models import load_model
-from PyQt5.QtCore import Qt, QTimer
-from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap, QImage, QFont
 from PyQt5.QtWidgets import QLabel, QWidget, QApplication
 from ..infer.steering_engine import SteeringEngine
 from ..infer.sliding_window_inference_engine import SlidingWindowInferenceEngine
@@ -21,12 +23,21 @@ SCALING_FACTOR = 8
 # The distance from the center to the edge of the green squares placed along the road line
 MARKER_RADIUS = 2
 
+# The ideal position for the center of the image
+IDEAL_CENTER_X = 120
+
+# Labels for each of the elements of an image data tuple
+IMAGE_DATA_LABELS = ('File name', 'Predicted center', 'Error from center', 'Steering angle')
+
 
 # Main PyQt5 QWidget class
 class Visualizer(QWidget):
 
     # List of NumPy images
-    image_lists = None
+    image_list = None
+
+    # List of image file names
+    image_data = []
 
     # The image we are currently on
     image_index = 0
@@ -71,7 +82,7 @@ class Visualizer(QWidget):
             inference_engines.append(inference_engine)
 
         # Load and perform inference on the images
-        self.image_list = load_images(inference_engines, image_folder)
+        self.image_list, self.image_data = load_images(inference_engines, image_folder)
 
         # Set the global image height and width variables
         image_height, image_width = self.image_list[0].shape[:2]
@@ -101,10 +112,10 @@ class Visualizer(QWidget):
         self.show()
 
         # Display the initial image
-        self.update_display()
+        self.update_display(1)
 
     # Update the image in the display
-    def update_display(self):
+    def update_display(self, num_frames):
 
         # Get the image that we should display from the list
         image = self.image_list[self.image_index]
@@ -118,15 +129,36 @@ class Visualizer(QWidget):
         # Fill the image box with the picture
         self.image_box.setPixmap(current_image_qpixmap)
 
-        # Update the index of the current image
-        self.image_index += 1
+        # Print a blank line to separate this image from the last
+        print()
+
+        # Print some metadata about the image, with the labels provided in IMAGE_DATA_LABELS
+        for name, value in zip(IMAGE_DATA_LABELS, self.image_data[self.image_index]):
+
+            # Print the name and value in a single line
+            print(name, value, sep=': ')
+
+        # Update the index of the current image by whatever number is provided
+        self.image_index += num_frames
 
         # If it has passed the last image, reset it to 0
         if self.image_index == len(self.image_list):
             self.image_index = 0
 
-        # Update again in 30 milliseconds
-        QTimer().singleShot(30, self.update_display)
+    # Listen for key presses and update the display
+    def keyPressEvent(self, event):
+
+        # If the key is the left arrow key
+        if event.key() == Qt.Key_Left:
+
+            # Go to the next frame
+            self.update_display(1)
+
+        # If it is the right arrow key
+        elif event.key() == Qt.Key_Right:
+
+            # Go to the previous frame
+            self.update_display(-1)
 
 
 # Load and process the image with the provided inference engine
@@ -139,14 +171,24 @@ def load_images(inference_engines, image_folder):
     steering_engine = SteeringEngine(
         max_average_variation=20,
         steering_multiplier=0.1,
-        ideal_center_x=160
+        ideal_center_x=IDEAL_CENTER_X,
+        steering_limit=0.2
     )
 
     # List that we will add processed images to
     image_list = []
 
+    # List of file names of images
+    image_names = sorted(os.listdir(image_folder))
+
+    # List of image metadata for display
+    image_data = []
+
     # Loop over each of the images in the folder
-    for image_name in sorted(os.listdir(image_folder)):
+    for image_name in image_names:
+
+        # Print out the file name of the image
+        print('Loaded {}'.format(image_name))
 
         # Load the image from disk, using its fully qualified path
         image_path = image_folder + '/' + image_name
@@ -162,34 +204,44 @@ def load_images(inference_engines, image_folder):
             line_positions.append(inference_engine.infer(image))
 
         # Calculate a steering angle from the points
-        steering_angle = steering_engine.compute_steering_angle(*line_positions)
+        steering_angle, error = steering_engine.compute_steering_angle(*line_positions)
 
         # Calculate the center of the road from the steering angle
-        error = steering_angle / steering_engine.steering_multiplier
         center_x = int(steering_engine.ideal_center_x - error)
 
-        # Flatten the list of line positions
-        flat_line_positions = [item for sublist in line_positions for item in sublist]
+        # Create a vertical blue line at the same X position as the predicted center of the road
+        image[:, center_x] = [0, 0, 255]
 
-        # Add the center of the road to the list of positions, at height 20
-        flat_line_positions.append((center_x, 20))
+        # Create a vertical black line at the predefined center of the image
+        image[:, IDEAL_CENTER_X] = 0
 
-        # For each of the positions which include horizontal and vertical values
-        for position in flat_line_positions:
+        # For each of the two road lines
+        for i in range(2):
 
-            # Calculate the four bounds of the marker to be placed
-            bounds = [center + offset for center in position for offset in (-MARKER_RADIUS, MARKER_RADIUS)]
+            # Define a color that has the ith element set to 255 and the others to 0
+            color = [0] * 3
+            color[i] = 255
 
-            # Create a black square within the bounds
-            image[bounds[2]:bounds[3], bounds[0]:bounds[1]] = 0
+            # For each of the positions which include horizontal and vertical values
+            for position in line_positions[i]:
+
+                # Calculate the four bounds of the marker to be placed
+                bounds = [center + offset for center in position for offset in (-MARKER_RADIUS, MARKER_RADIUS)]
+
+                # Create a black square within the bounds
+                image[bounds[2]:bounds[3], bounds[0]:bounds[1]] = color
 
         # Add the prepared image to the list
         image_list.append(image)
 
+        # Add the corresponding name, center position, error, and steering angle to the data list
+        image_data.append((image_name, center_x, error, steering_angle))
+
     # Notify the user that loading is complete
     print('Loading complete!')
 
-    return image_list
+    # Return the images and their corresponding names
+    return image_list, image_data
 
 
 # If this file is being run directly, instantiate the ManualSelection class
