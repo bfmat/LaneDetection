221d753ac652835b6ff0ef437bf1b35c2e07a295
brendon-ai <brendon-m@outlook.com>
2017-09-20 14:08:38 -0400

    str4

--- test/speed_test.py
+++ test/speed_test.py
@@ -33,7 +33,7 @@ image_path = os.path.expanduser(sys.argv[3])
 inference_engine = SlidingWindowInferenceEngine(
     model=model,
     window_size=16,
-    stride=(8, 16)
+    stride=(16, 8)
 )
 
 # Record the starting time
