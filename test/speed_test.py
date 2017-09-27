754b0047ddd9485f65d9bb16322106d921af1412
brendon-ai <brendon-m@outlook.com>
2017-09-27 12:24:06 -0400

    Use horizontal stride of 4, for speed testing

--- test/speed_test.py
+++ test/speed_test.py
@@ -33,7 +33,7 @@ image_path = os.path.expanduser(sys.argv[3])
 inference_engine = SlidingWindowInferenceEngine(
     model=model,
     window_size=16,
-    stride=8
+    stride=(8, 4)
 )
 
 # Record the starting time
