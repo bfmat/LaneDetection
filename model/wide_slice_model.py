e00dd50e7be5bc71c1ec9abaf2091f823017e612
brendon-ai <brendon-m@outlook.com>
2017-10-01 11:26:26 -0400

    Switched to functional model for wide slice model

--- model/wide_slice_model.py
+++ model/wide_slice_model.py
@@ -1,5 +1,5 @@
-from keras.models import Sequential
-from keras.layers import Dense, Flatten
+from keras.models import Model, Sequential
+from keras.layers import Input, Dense, Flatten
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.convolutional import Conv2D
 
@@ -13,38 +13,47 @@ def wide_slice_model(slice_dimensions):
     # Hyperbolic tangent activation function
     activation = 'tanh'
 
-    # Initialize the sequential model
-    model = Sequential()
+    # Initialize the sequential format
+    x = Sequential()
 
     # Input shape is provided, but add the channel axis to it
     input_shape = slice_dimensions + (3,)
 
+    # Define the input
+    input_layer = Input(shape=input_shape)
+
     # Four convolutional layers
-    model.add(Conv2D(
+    x = Conv2D(
         input_shape=input_shape,
         kernel_size=4,
         filters=10,
         activation=activation
-    ))
+    )(input_layer)
 
-    model.add(Conv2D(
+    x = Conv2D(
         kernel_size=3,
         filters=32,
         activation=activation
-    ))
+    )(x)
 
-    model.add(Conv2D(
+    x = Conv2D(
         kernel_size=3,
         filters=108,
         activation=activation
-    ))
+    )(x)
 
-    model.add(MaxPooling2D(pool_size=3))
+    x = MaxPooling2D(pool_size=3)(x)
 
     # Fully connected layers
-    model.add(Flatten())
-    model.add(Dense(128, activation=activation))
-    model.add(Dense(1))
+    x = Flatten()(x)
+    x = Dense(128, activation=activation)(x)
+    output_layer = Dense(1)(x)
+
+    # Define the model
+    model = Model(
+        inputs=input_layer,
+        outputs=output_layer
+    )
 
     # Compile model with Adadelta optimizer
     model.compile(
