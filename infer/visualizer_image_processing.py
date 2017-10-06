dbda82438a599c8ec4540c0134d76c9078650d0c
brendon-ai <brendon-m@outlook.com>
2017-10-06 14:03:24 -0400

    Tile the heat map views with the main view

--- infer/visualizer_image_processing.py
+++ infer/visualizer_image_processing.py
@@ -3,7 +3,7 @@ from __future__ import division, print_function
 import os
 import numpy
 
-from scipy.misc import imread
+from scipy.misc import imread, imresize
 from ..infer.steering_engine import remove_outliers
 
 
@@ -13,8 +13,49 @@ from ..infer.steering_engine import remove_outliers
 # Created by brendon-ai, October 2017
 
 
+# Load and process the image with the provided inference engines and steering engine
+def process_images(image_folder, inference_engines, steering_engine, marker_radius):
+
+    # Notify the user that we have started loading the images
+    print('Loading images...')
+
+    # List that we will add processed images to
+    image_list = []
+
+    # List of file names of images
+    image_names = sorted(os.listdir(image_folder))
+
+    # List of image metadata for display
+    all_image_data = []
+
+    # Loop over each of the images in the folder
+    for image_name in image_names:
+
+        # Print out the file name of the image
+        print('Loaded {}'.format(image_name))
+
+        # Load the image from disk, using its fully qualified path
+        image_path = image_folder + '/' + image_name
+        image = imread(image_path)
+
+        # Process the image and add various markings to it, recording metadata returned for display purposes
+        processed_image, image_data = _process_single_image(image, inference_engines, steering_engine, marker_radius)
+
+        # Add the prepared image to the list
+        image_list.append(processed_image)
+
+        # Add the corresponding name, center position, error, and steering angle to the data list
+        all_image_data.append((image_name,) + image_data)
+
+    # Notify the user that loading is complete
+    print('Loading complete!')
+
+    # Return the images and their corresponding names
+    return image_list, all_image_data
+
+
 # Perform all necessary processing on a single image to prepare it for visualization
-def process_single_image(image, inference_engines, steering_engine, marker_radius):
+def _process_single_image(image, inference_engines, steering_engine, marker_radius):
 
     # List of points on the lines
     all_line_positions = []
@@ -54,20 +95,8 @@ def process_single_image(image, inference_engines, steering_engine, marker_radiu
         # Add it to the outlier-only list
         all_line_positions_outliers_only.append(line_positions_outliers_only)
 
-    # Display a heat map in place on the image
-    apply_heat_map(image, inference_engines[0].last_prediction_tensor)
-
     # Calculate the center of the road from the steering angle
     center_x = int(steering_engine.ideal_center_x - error)
-
-    # Create a vertical blue line at the same X position as the predicted center of the road, if possible
-    try:
-        image[:, center_x] = [0, 0, 255]
-    except:
-        pass
-
-    # Create a vertical black line at the predefined center of the image
-    image[:, steering_engine.ideal_center_x] = 0
 
     # Combine the two road lines with the lists of outliers, along with their corresponding colors
     lines_and_colors = [
@@ -76,6 +105,38 @@ def process_single_image(image, inference_engines, steering_engine, marker_radiu
         (all_line_positions_outliers_only[0], [255, 0, 0]),
         (all_line_positions_outliers_only[1], [255, 255, 0])
     ]
+
+    # Copy the image twice for use in the heat map section of the user interface
+    heat_map_images = [numpy.copy(image) for _ in range(2)]
+
+    # Apply the heat map in place to the copied images, using a different inference engine for each
+    for heat_map_image, inference_engine in zip(heat_map_images, inference_engines):
+        _apply_heat_map(heat_map_image, inference_engine.last_prediction_tensor)
+
+    # Downscale the heat map images to half of their original size
+    downscaled_heat_map_images = [imresize(heat_map_image, 0.5) for heat_map_image in heat_map_images]
+
+    # Add the relevant lines and points to the original image
+    _add_markers(image, steering_engine, marker_radius, center_x, lines_and_colors)
+
+    # Concatenate the two small images together and then concatenate them to the main image
+    concatenated_heat_map_image = numpy.concatenate(downscaled_heat_map_images, axis=1)
+    tiled_image = numpy.concatenate((image, concatenated_heat_map_image), axis=0)
+
+    # Return relevant metadata about the image as well as the image itself
+    return tiled_image, (center_x, error, steering_angle)
+
+
+# Add lines and points to an image
+def _add_markers(image, steering_engine, marker_radius, center_x, lines_and_colors):
+    # Create a vertical blue line at the same X position as the predicted center of the road, if possible
+    try:
+        image[:, center_x] = [0, 0, 255]
+    except:
+        pass
+
+    # Create a vertical black line at the predefined center of the image
+    image[:, steering_engine.ideal_center_x] = 0
 
     # For each of the two road lines
     for line_positions, color in lines_and_colors:
@@ -89,12 +150,9 @@ def process_single_image(image, inference_engines, steering_engine, marker_radiu
             # Create a black square within the bounds
             image[bounds[2]:bounds[3], bounds[0]:bounds[1]] = color
 
-    # Return relevant metadata about the image
-    return center_x, error, steering_angle
-
 
 # Display a translucent red heat map over an image (modifying it in place), given a tensor of predictions to base it on
-def apply_heat_map(image, prediction_tensor):
+def _apply_heat_map(image, prediction_tensor):
 
     # Find the factor to calculate rectangular blocks in the image
     # that visually correspond to the positions in the prediction tensor
@@ -118,44 +176,3 @@ def apply_heat_map(image, prediction_tensor):
         red_block = numpy.zeros(block.shape)
         red_block[:, :, 0] = 255 * red_weight
         block[:] = block * (1 - red_weight) + red_block
-
-
-# Load and process the image with the provided inference engines and steering engine
-def process_images(image_folder, inference_engines, steering_engine, marker_radius):
-
-    # Notify the user that we have started loading the images
-    print('Loading images...')
-
-    # List that we will add processed images to
-    image_list = []
-
-    # List of file names of images
-    image_names = sorted(os.listdir(image_folder))
-
-    # List of image metadata for display
-    all_image_data = []
-
-    # Loop over each of the images in the folder
-    for image_name in image_names:
-
-        # Print out the file name of the image
-        print('Loaded {}'.format(image_name))
-
-        # Load the image from disk, using its fully qualified path
-        image_path = image_folder + '/' + image_name
-        image = imread(image_path)
-
-        # Process the image and add various markings to it, recording metadata returned for display purposes
-        image_data = process_single_image(image, inference_engines, steering_engine, marker_radius)
-
-        # Add the prepared image to the list
-        image_list.append(image)
-
-        # Add the corresponding name, center position, error, and steering angle to the data list
-        all_image_data.append((image_name,) + image_data)
-
-    # Notify the user that loading is complete
-    print('Loading complete!')
-
-    # Return the images and their corresponding names
-    return image_list, all_image_data
