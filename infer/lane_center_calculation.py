2db8712936f5d8536894739f6b4b72286aff7b2a
brendon-ai <brendon-m@outlook.com>
2017-10-10 17:04:55 -0400

    Completed lane center calculation file, untested

--- infer/lane_center_calculation.py
+++ infer/lane_center_calculation.py
@@ -10,30 +10,34 @@ import math
 # Calculate the center line of a tensor of predictions of an arbitrary size, with a minimum confidence for the line
 # and scale it so that the output maps onto locations in the original source image
 def calculate_lane_center_positions(prediction_tensor, minimum_prediction_confidence,
-                                    original_image_width, original_window_width):
+                                    original_image_shape, window_size):
 
     # Add the center points of the rows to a list
     center_positions = []
 
     # Iterate over the rows
-    for row in prediction_tensor:
+    for y_position in range(len(prediction_tensor)):
 
         # Find the peak in both directions from the last center position
         peak_indices = [find_peak_in_direction
-                        (row, center_positions[-1], reversed_iteration_direction, minimum_prediction_confidence)
+                        (prediction_tensor[y_position], center_positions[-1], reversed_iteration_direction, minimum_prediction_confidence)
                         for reversed_iteration_direction in (False, True)]
 
         # If a peak could be found in both directions
         if None not in peak_indices:
 
-            # Calculate the average of the two peaks
-            center_position = sum(peak_indices) / len(peak_indices)
+            # Calculate the average of the two peaks and add the Y position of the row to the tuple
+            center_x_position = sum(peak_indices) / len(peak_indices)
+            center_position = (y_position, center_x_position)
 
             # Scale and offset it so that it corresponds to the correct position within the original image
-            width_scaling_factor =
-            center_position_scaled = (center_position * width_scaling_factor) - original_window_width
+            center_position_scaled = [center_position_element * (image_shape_element / prediction_tensor_shape_element)
+                                      for center_position_element, image_shape_element, prediction_tensor_shape_element
+                                      in zip(center_position, original_image_shape, prediction_tensor.shape)]
+            center_position_offset = [element + window_size for element in center_position_scaled]
 
-            center_positions.append(center_position)
+            # Add the processed position to the list
+            center_positions.append(center_position_offset)
 
     return center_positions
 
