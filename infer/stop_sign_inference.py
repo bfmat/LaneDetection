e8a308c1a0fea1839a1a4558cccafc91ae8f3f89
brendon-ai <brendon-m@outlook.com>
2018-04-26 16:33:20 -0400

    Bounding box sizes are correct!

--- infer/stop_sign_inference.py
+++ infer/stop_sign_inference.py
@@ -7,10 +7,10 @@ from skimage.util import view_as_windows
 
 # Side length of the square windows to cut the images into
 WINDOW_SIZE = 16
-# Stride to move the window by along both dimensions
-STRIDE = 1
 # Minimum area for a blob to be recognized
 MIN_AREA = 16
+# Number of pixels between the center and the edge of a bounding box, per size unit of the corresponding blob
+BOX_SCALE = 0.7
 
 
 def box_stop_signs(model, search_image, draw_image):
@@ -28,7 +28,7 @@ def box_stop_signs(model, search_image, draw_image):
 
     # Slice up the image into windows
     image_slices = view_as_windows(
-        search_image, (WINDOW_SIZE, WINDOW_SIZE, 3), STRIDE)
+        search_image, (WINDOW_SIZE, WINDOW_SIZE, 3))
     # Flatten the window array so that there is only one non-window dimension
     image_slices_flat = np.reshape(
         image_slices, (-1,) + image_slices.shape[-3:])
@@ -43,17 +43,23 @@ def box_stop_signs(model, search_image, draw_image):
     # Find blobs in the heat map, which will be located around the location of the stop signs
     blob_key_points = blob_detector.detect(predictions_integer)
     # Convert the key points to tuple positions
-    blob_positions = [key_point.pt for key_point in blob_key_points]
+    blob_positions, blob_sizes = zip(*((key_point.pt, key_point.size)
+                                       for key_point in blob_key_points))
     # Transpose the shapes of the heat map and the image into (X, Y) format
     heat_map_shape, image_shape = [
         (array.shape[1], array.shape[0])
         for array in [predictions_integer, draw_image]
     ]
+    # Reduce the image's effective size by the window size, to represent the areas on the edges that the windows are never centered at
+    image_shape = tuple(shape_element - WINDOW_SIZE
+                        for shape_element in image_shape)
 
     # Scale the blob positions to correspond to positions on the original image
+    # Increase all values by half of the window size, so the reduced image size is inset into the available space
     blob_positions_scaled = [
         [
-            (position_element / heat_map_dimension) * image_dimension
+            (position_element / heat_map_dimension)
+            * image_dimension + (WINDOW_SIZE // 2)
             for position_element, heat_map_dimension, image_dimension
             in zip(position, heat_map_shape, image_shape)
         ]
@@ -61,8 +67,9 @@ def box_stop_signs(model, search_image, draw_image):
     ]
 
     # Draw each of the blobs on the output image
-    for position in blob_positions_scaled:
-        half_edge_length = 10
+    for position, size in zip(blob_positions_scaled, blob_sizes):
+        # Multiply the size of the blob by the scale factor to get the distance between the center and the edge of the box
+        half_edge_length = size * BOX_SCALE
         # Get the positions of the top left and bottom right corners of the bounding box
         top_left = tuple(int(position_element - half_edge_length)
                          for position_element in position)
@@ -73,6 +80,5 @@ def box_stop_signs(model, search_image, draw_image):
             img=draw_image,
             pt1=top_left,
             pt2=bottom_right,
-            color=0,
-            thickness=2
+            color=(255, 128, 0)
         )
